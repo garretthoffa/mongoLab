9109600a922a3a963ae8a9c0fb8f084846df5344
garretthoffa <hoffa@cs.byu.edu>
2017-11-14 22:35:38 +0000

    working onr javascript

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,17 @@
-ngular.module('comment',[])
+angular.module('app',[])
 .controller('MainCtrl',[
-   '$scope',
-   function($scope)
+   '$scope','$http',
+   function($scope,$http)
    {
+	$scope.traffic =[];
+	$scope.addTraffic = function()
+	{
+	};
+	$scope.getAll = function() {
+	 return http.get('/traffic').success(function(data){
+          angular.copy(data, $scope.traffic);
+	});
+	$scope.getAll();
+   	}
    }
    ]);
