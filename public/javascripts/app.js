d668bda778dd282e5864f495dde3664b55ac6f57
garretthoffa <hoffa@cs.byu.edu>
2017-11-14 23:43:38 +0000

    here is stuff

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,17 +1,40 @@
-angular.module('app',[])
-.controller('MainCtrl',[
-   '$scope','$http',
-   function($scope,$http)
-   {
-	$scope.traffic =[];
-	$scope.addTraffic = function()
-	{
-	};
-	$scope.getAll = function() {
-	 return http.get('/traffic').success(function(data){
-          angular.copy(data, $scope.traffic);
-	});
-	$scope.getAll();
-   	}
-   }
-   ]);
+var app = window.angular.module('app',[])
+
+app.factory('trafficFetcher', trafficFetcher)
+app.controller('MainCtrl', mainCtrl)
+
+function trafficFetcher ($http)
+{
+ var API_ROOT = 'traffic'
+  return {
+    get: function () {
+      return $http
+        .get(API_ROOT)
+        .then(function (resp) {
+          return resp.data
+        })
+    },
+        post: function (formData) {
+      return $http
+         .post(API_ROOT,formData)
+         .then(function (resp) {
+           console.log("Post worked");
+         })
+    }
+  }
+}
+
+function mainCtrl ($scope,trafficFetcher)
+{
+	$scope.traffic = []
+	$scope.addTraffic = function() {
+		var formData={Time:$scope.Time,Location:$scope.Location,Description:$scope.Description};
+		console.log(formData);
+		trafficFetcher.post(formData);
+		$scope.traffic.push(formData);
+	}
+	trafficFetcher.get()
+	  .then(function (data) {
+		$scope.traffic = data
+	})
+}
